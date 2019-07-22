0b619f36a9d1612f23a65b1319872ec5b9c2faba
Csonka-Kis Andrea <cskand@gmail.com>
2019-07-22 15:58:00 +0200

    members layout

--- js/got.js
+++ js/got.js
@@ -1,32 +1,44 @@
 const members = {
-    data: [],
-    init() {
-        this.findAll();
-    },
-    findAll() {
-        const request = new XMLHttpRequest(); // ez egy szinkron művelet
-        request.onload = () => {
-            this.setData(request.responseText); // innéttől kezdve lesz benne adat
-        };
-        request.onerror = () => {
-            alert('hiba a mátrixban');
-        };
-        request.open('GET', '/json/got.json');
-        request.send();
-    },
-    setData(userData) {
-        this.data = JSON.parse(userData);
-    },
-    showOneRow(user) {
-        let row = `<div data-id="${user.id}">`;
-        for (let k in user) {
-            if (user.hasOwnProperty(k)) {
-                row += `<td>${user[k]}</td>`;
-            }
-        }
-        row += `<td><button onclick ="userController.remove(${user.id})">Töröl</button></td> <td><button onclick ="userController.edit(${user.id})">Semmisejó</button></td></div>`;
-        return row;
-    },
+  data: [],
+  init() {
+    this.findAll();
+  },
+  findAll() {
+    const request = new XMLHttpRequest(); // ez egy szinkron művelet
+    request.onload = () => {
+      this.setData(request.responseText); // innéttől kezdve lesz benne adat
+    };
+    request.onerror = () => {
+      alert('hiba a mátrixban');
+    };
+    request.open('GET', '/json/got.json');
+    request.send();
+  },
+  setData(userData) {
+    this.data = JSON.parse(userData);
+    this.filterAliveMembers();
+  },
+
+  filterAliveMembers() {
+    for (i = 0; i < this.data.length; i += 1) {
+      if (this.data[i].dead === true) {
+        this.data.splice(i, 1);
+      }
+    }
+    this.showOneRow();
+  },
+
+  showOneRow(data) {
+    let row = '';
+    for (i = 0; i < 48; i += 1) {
+      if (this.data.hasOwnProperty(i)) {
+        row += `<div class="small__pictures" id="${[i]}"><div><img src="${this.data[i].portrait} " alt="" class="images"></div>
+                <div class="image__names">${this.data[i].name}</div></div>`;
+      }
+    }
+    document.querySelector('.pictures__container').innerHTML = row;
+    return row;
+  },
 }
 
 members.init();
